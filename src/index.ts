ccd7ad431d9df5859893242284db5cd6571b42b1
Lucas C <lucas@lucasc.me>
2024-06-21 14:35:25 -0500

    Delete src directory

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Namespace } from "./helpers/Namespace";
-
-/* Build the structure of the SDK */
-
-//Merge the local controls into the 'atlas.control' namespace.
-import * as baseControl from "./control";
-const control = Namespace.merge("atlas.control", baseControl);
-export { control };
